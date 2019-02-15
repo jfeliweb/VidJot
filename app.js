69378bb7a6c5ea5b45f29d880443ff00b1b6f62e
MacBaby\Jeazy <jeazy@bizdotsmedia.com>
2019-02-15 15:53:40 -0500

    add edit logic

--- app.js
+++ app.js
@@ -62,6 +62,18 @@ app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
 });
 
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    res.render('ideas/edit', {
+      idea: idea
+    });
+  });
+});
+
 // Process Idea Form
 app.post('/ideas', (req, res) => {
   let errors = [];
